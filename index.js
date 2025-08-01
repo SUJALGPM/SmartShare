bce4f6fc84286a03cf0981f511eb3dcfcf221410
Sujal <mailme.sujaldingankar@gmail.com>
2025-08-01 23:49:17 +0530

    index.js revert back to render

--- index.js
+++ index.js
@@ -1,52 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const colors = require('colors');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const methodOverride = require('method-override');
-// const { connectDB } = require('./Config/Db');
-// const CodeRoute = require('./Routes/CodeRoute');
-// const FileRoute = require("./Routes/FileRoute");
-
-// //Configure dotenv file...
-// dotenv.config();
-
-
-// //Configure Database connectin file...
-// connectDB();
-
-
-// //Configure Object Files...
-// const app = express();
-
-
-// //Setup Middlewares...
-// // app.use(express());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(methodOverride('_method'));
-// app.set('view engine', 'ejs');
-
-
-// //Configure Routes...
-// app.use('/api', CodeRoute);
-// app.use('/api', FileRoute);
-
-
-// //Configure Server Port...
-// const port = process.env.PORT || 9001;
-
-
-// //Setup Server port...
-// app.listen(port, () => {
-//     console.log(`Server is successfully running at port no : ${process.env.PORT}`.bgCyan.white);
-// })
-
-
-
-
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -57,32 +8,41 @@ const { connectDB } = require('./Config/Db');
 const CodeRoute = require('./Routes/CodeRoute');
 const FileRoute = require("./Routes/FileRoute");
 
-// Load .env
+//Configure dotenv file...
 dotenv.config();
 
-// Connect to DB
+
+//Configure Database connectin file...
 connectDB();
 
-// Create Express app
+
+//Configure Object Files...
 const app = express();
 
-// Middleware
+
+//Setup Middlewares...
+// app.use(express());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(cors({
-  origin: ["http://localhost:3000", "https://zapshare.netlify.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
-app.options("*", cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
-// Routes
+
+//Configure Routes...
 app.use('/api', CodeRoute);
 app.use('/api', FileRoute);
 
-module.exports = app;
+
+//Configure Server Port...
+const port = process.env.PORT || 9001;
+
+
+//Setup Server port...
+app.listen(port, () => {
+    console.log(`Server is successfully running at port no : ${process.env.PORT}`.bgCyan.white);
+})
+
+
+
